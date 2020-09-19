ce481e585a0d99d54cd96261be5cbd636e2a7f9b
jblandon90 <jeisonbladovel@gmail.com>
2020-09-19 16:55:40 -0500

    routs

--- larapp/routes/web.php
+++ larapp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
+});
+
+
+Route::post('helloword', function () {
+    return "<h1>Hola</h1>";
+   
+});
+
+Route::get('user/{id}', function ($id) {
+    dd(App\User::find($id));
+   
+});
+
+Route::get('edades', function () {
+   $users = App\User::all()->take(10);
+    
+   foreach ($users as $user) {
+
+    $edad = Carbon::parse($user->birthdate)->age;
+    $creacion = new Carbon($user->created_at);
+    $creacion->setlocale('es');
+    $tiempo = $creacion->diffForHumans();
+    
+    dump($user->fullname.' tiene '.$edad.' años'.' se creo '.$tiempo);
+   }
+   
 });
